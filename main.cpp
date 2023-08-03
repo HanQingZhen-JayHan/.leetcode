78cd940a5641325cf3e65f33ed51c69f692eb767
JayHan <jayhancanada@gmail.com>
2023-08-03 13:34:58 -0700

    design twitter

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@
 // #include "13.roman-to-integer.cpp"
 // #include "12.integer-to-roman.cpp"
 #include "126.word-ladder-ii.cpp"
-#include "15.3-sum.cpp"
-#include "79.word-search.cpp"
-#include "71.simplify-path.cpp"
 #include "139.word-break.cpp"
+#include "15.3-sum.cpp"
 #include "152.maximum-product-subarray.cpp"
+#include "355.design-twitter.cpp"
 #include "672.bulb-switcher-ii.cpp"
-int main(int, char **) {
+#include "71.simplify-path.cpp"
+#include "79.word-search.cpp"
+int main(int, char**) {
 
-  print("Hello, from LeetCode!");
-  Solution672 s;
-  s.test();
+    print("Hello, from LeetCode!");
+    // Solution672 s;
+    Twitter s;
+    s.test();
 }
